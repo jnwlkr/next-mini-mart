c142b7cea2d21d0afcf3b0d1bd78be673ca682d4
Jen Walker <jennie.walker@uconn.edu>
2021-08-16 15:29:30 -0700

    added setup for sorting

--- components/ProductList/ProductList.js
+++ components/ProductList/ProductList.js
@@ -4,6 +4,10 @@ import { Box } from '@chakra-ui/react';
 export default function ProductList({ products }) {
     if (!products) return null;
 
+/*     function sortPrice({ products }) {
+        products.sort((a, b) => a.price - b.price);
+    } */
+
     return (
         <Box 
             display='flex' 
