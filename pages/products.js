8ad1683efef06fb3c6756020bf878ecec4706b19
Jen Walker <jennie.walker@uconn.edu>
2021-08-16 12:36:09 -0700

    sort by button

--- pages/products.js
+++ pages/products.js
@@ -1,13 +1,40 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Menu, MenuButton, MenuList, MenuItem, Button } from '@chakra-ui/react';
 import { Navbar } from '../components';
+import { ChevronDownIcon } from '@chakra-ui/icons';
 
 const Products = () => {
 
     return (
-        <Box>
-            <Navbar textColor='black' />
+        <>
+        <Navbar textColor='black' />
+        <Box mx={5} mt={1}>
+            <Box
+                w='100%'
+                display='flex'
+                alignItems='center'
+                justifyContent='flex-end'
+                p={5}
+            >
+                <Menu>
+                    <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+                        Sort by
+                    </MenuButton>
+                    <MenuList>
+                        <MenuItem>
+                            Price: Low to high
+                        </MenuItem>
+                        <MenuItem>
+                            Price: High to low
+                        </MenuItem>
+                        <MenuItem>
+                            Name: A-Z
+                        </MenuItem>
+                    </MenuList>
+                </Menu>
+            </Box>
         
         </Box>
+        </>
     );
 };
 
