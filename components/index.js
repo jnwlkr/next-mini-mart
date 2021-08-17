22bfbe2751f6b7b524d0dd0aa4eb4102766e3d4b
Jen Walker <jennie.walker@uconn.edu>
2021-08-17 13:51:41 -0700

    Category and CategoryList exports

--- components/index.js
+++ components/index.js
@@ -1,3 +1,5 @@
 export {default as Navbar} from './Navbar/Navbar';
 export {default as Product} from './Product/Product';
 export {default as ProductList} from './ProductList/ProductList';
+export {default as Category} from './Category';
+export {default as CategoryList} from './CategoryList';
