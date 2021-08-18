58627c9fc4bcc52139946641b4860a3576bc2b66
Jen Walker <jennie.walker@uconn.edu>
2021-08-18 12:07:15 -0700

    added details and adjusted styles

--- pages/products/[permalink].js
+++ pages/products/[permalink].js
@@ -3,8 +3,9 @@ import Image from 'next/image';
 import { Navbar } from '../../components';
 import commerce from '../../lib/commerce';
 import { ChevronRightIcon } from '@chakra-ui/icons';
-import { Box, Button, Breadcrumb, BreadcrumbItem, Text, Divider, useToast } from '@chakra-ui/react';
+import { Box, Button, Breadcrumb, BreadcrumbItem, Text, Divider, useToast, Icon, VStack } from '@chakra-ui/react';
 import { useCartDispatch } from '../../context/cart';
+import { MdCheckCircle, MdLocalShipping, MdMood } from 'react-icons/md';
 
 export async function getStaticProps({ params }) {
     const { permalink } = params;
@@ -69,17 +70,28 @@ export default function ProductPage({ product }) {
                 </Breadcrumb>
             </Box>
             <Box display='flex' flexWrap='wrap' justifyContent='space-around' mx={10} mt={5}>
-                <Box display='flex' alignItems='center' justifyContent='center' maxWidth={{ base: '100%', md: '50%'}}>
-                    <Image width={400} height={400} src={product.media.source} alt='product image' />
+                <Box display='flex' alignItems='center' justifyContent='center' p={{base: 0, md: 5}} maxWidth={{ base: '100%', md: '50%'}}>
+                    <Image width={500} height={500} src={product.media.source} alt='product image' />
                 </Box>
-                <Box display='flex' flexDirection='row' flexWrap='wrap' alignContent='flex-start' maxWidth={{ base: '100%', md: '50%'}}>
+                <Box display='flex' flexWrap='wrap' alignContent='flex-start' p={{base: 0, md: 5}} maxWidth={{ base: '100%', md: '50%'}}>
                     <Box w='100%' display='flex' flexWrap='wrap' justifyContent='space-between' alignItems='center'>
-                        <Text fontSize='3xl' fontWeight={300}>{product.name}</Text>
-                        <Text fontSize='xl' fontWeight={600}>{product.price.formatted_with_symbol}</Text>
+                        <Text fontSize='4xl' fontWeight={600}>{product.name}</Text>
+                        <Text fontSize='lg' fontWeight={400}>{product.price.formatted_with_symbol}</Text>
                     </Box>
                     <Divider />
-                    <Box my={10} display='flex'>
-                        <Text fontSize='sm' fontWeight={300} dangerouslySetInnerHTML={{ __html: product.description }} />
+                    <Box display='flex' flexWrap='wrap' mb={5} width='100%'>
+                        <Text fontSize='sm' my={5} fontWeight={300} dangerouslySetInnerHTML={{ __html: product.description }} />
+                        <Box display='flex' fontWeight={400} fontSize='sm' p={5} flexDirection='column' justifyContent='space-between' borderRadius='10px' bgColor='gray.100' width='100%' height='130px'>
+                            <Box>
+                                <Icon mr={2} w={7} h={7} as={MdCheckCircle} color='green.400' /> In stock
+                            </Box>
+                            <Box>
+                                <Icon mr={2} w={7} h={7} as={MdLocalShipping} /> Available for shipping
+                            </Box>
+                            <Box>
+                                <Icon mr={2} w={7} h={7} as={MdMood} /> 125% Satisfaction Guarantee
+                            </Box>
+                        </Box>
                     </Box>
                     <Button isFullWidth onClick={addToCart}>Add to Cart</Button>
                 </Box>
