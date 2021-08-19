fe4e70a4116995ac7043260048cbdd32fd43d1d7
Jen Walker <jennie.walker@uconn.edu>
2021-08-18 18:32:51 -0700

    cart layout

--- pages/cart.js
+++ pages/cart.js
@@ -1,9 +1,11 @@
 import { useCartState, useCartDispatch } from '../context/cart';
 import commerce from '../lib/commerce';
-import { Box, Button } from '@chakra-ui/react';
+import { Box, Button, Text, Flex, VStack } from '@chakra-ui/react';
 import { Navbar } from '../components';
+import Image from 'next/image';
+import Link from 'next/link';
 
-function CartItem({ id, name, quantity, line_total }) {
+function CartItem({ id, name, quantity, line_total, media, permalink }) {
     const { setCart } = useCartDispatch();
 
     const handleUpdateCart = ({ cart }) => setCart(cart);
@@ -23,16 +25,19 @@ function CartItem({ id, name, quantity, line_total }) {
     }
 
     return (
-        <Box>
-            <Box>{name}</Box>
-            <Box>{quantity}</Box>
-            <Box>{line_total.formatted_with_symbol}</Box>
+        <Flex justifyContent='space-between' height='125px'>
             <Box>
-                <Button onClick={decrementQuantity}> - </Button>
-                <Button onClick={incrementQuantity}> + </Button>
+                <Link href={`/products/${permalink}`}>
+                    <a>
+                        <Text fontWeight={200}>{name}</Text>
+                    </a>
+                </Link>
+                <Text fontSize='xs'>Quantity: {quantity}</Text>
             </Box>
-            <Button onClick={removeItem}>&times;</Button>
-        </Box>
+            <Box>
+                <Text fontSize='sm' fontWeight={400}>{line_total.formatted_with_symbol}</Text>
+            </Box>
+        </Flex>
     );
 }
 
@@ -44,27 +49,26 @@ export default function CartPage() {
     return (
         <>
         <Navbar textColor='black' />
-        <Box>
-            {/* Header */}
-            Cart 
-
-            {isEmpty ? (
-                // Empty cart view
-                <Box>Your cart is empty</Box>
-            ) : (
-                // Items in cart view
-                <>
-                    {line_items.map((item) => (
-                        <CartItem key={item.id} {...item} />
-                    ))}
-                    <hr />
-                    <Box>
-                        Subtotal: {subtotal.formatted_with_symbol}
-                    </Box> 
-                </>
-            )}
-            
-        </Box>
+            <Text fontSize='2xl' px={10} mt={5} fontWeight={600}>Shopping Cart</Text>
+            <Flex px={10} flexWrap='wrap' justifyContent='space-between' width='100%'>
+                {isEmpty ? (
+                    <Box>Your cart is empty!</Box>
+                ) : (
+                    // Items in cart view
+                    <>
+                        <Flex flexDirection='column' justifyContent='space-between' minWidth={{base: '100%', md: '70%'}} p={5}>
+                            
+                            {line_items.map((item) => (
+                                <CartItem key={item.id} {...item} />
+                            ))}
+                        </Flex>
+                        <Flex minWidth={{base: '100%', md: '30%'}} p={5}>
+                            Subtotal: {subtotal.formatted_with_symbol}
+                        </Flex> 
+                    </>
+                    )
+                }
+            </Flex>
         </>
     );
 };
