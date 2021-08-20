007434355fe3815f67b4710a5d64a56211069988
Jen Walker <jennie.walker@uconn.edu>
2021-08-20 12:12:01 -0700

    cart items, padding

--- pages/cart.js
+++ pages/cart.js
@@ -1,53 +1,81 @@
 import { useCartState, useCartDispatch } from '../context/cart';
 import commerce from '../lib/commerce';
-import { Box, Button, Text, Flex, Divider, CloseButton, ButtonGroup} from '@chakra-ui/react';
+import { Box, Button, Text, Flex, Divider, CloseButton, ButtonGroup, useToast} from '@chakra-ui/react';
 import { Navbar } from '../components';
+import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 
 function CartItem({ id, name, quantity, line_total, media, permalink }) {
+    const toast = useToast();
     const { setCart } = useCartDispatch();
 
     const handleUpdateCart = ({ cart }) => setCart(cart);
 
-    const removeItem = () => commerce.cart.remove(id).then(handleUpdateCart);
+    const removeItem = () => {
+        commerce.cart.remove(id).then(handleUpdateCart);
+        toast({
+            title: 'Cart updated!',
+            description: `${name} was removed from your cart`,
+            status: 'success',
+            duration: 2000,
+            isClosable: true,
+        })
+    }
 
     const decrementQuantity = () => {
-        quantity > 1 
-            ? commerce.cart
-                .update(id, { quantity: quantity - 1 })
-                .then(handleUpdateCart)
-            : removeItem();
+        if (quantity > 1) {
+            commerce.cart.update(id, { quantity: quantity - 1 }).then(handleUpdateCart);
+            toast({
+                title: 'Cart updated!',
+                description: `${name} was removed from your cart`,
+                status: 'success',
+                duration: 2000,
+                isClosable: true,
+            })
+        } else {
+            removeItem();
+        }
     };
 
     const incrementQuantity = () => {
         commerce.cart.update(id, { quantity: quantity + 1 }).then(handleUpdateCart);
+        toast({
+            title: 'Cart updated!',
+            description: `${name} was added to your cart`,
+            status: 'success',
+            duration: 2000,
+            isClosable: true,
+        })
     }
 
     return (
-        <Flex justifyContent='space-between' alignItems='center' height='120px' p={3} m={2} borderRadius='md' boxShadow='base'>
-            <Box width='25%' mr={2}>
+        <>
+        <Flex justifyContent='space-between' alignItems='center' minHeight={{base: '95px', sm: '140px', md: '150px'}} borderRadius='md' px={3} >
+            <Box width='20%' mr={2}>
                 <Link href={`/products/${permalink}`}>
                     <a>
-                        <Image width={80} height={80} src={media.source} alt='cart-item' />
+                        <Image width={120} height={120} src={media.source} alt='cart-item' />
                     </a>
                 </Link>
             </Box>
-            <Box width='30%'>
-                <Text fontSize='sm' fontWeight={200}>{name}</Text>
+            <Flex direction='column' width='35%'>
+                <Text fontSize={{base: 'xs', md: 'md'}} fontWeight={400}>{name}</Text>
                 <ButtonGroup size='xs' isAttached alignItems='center'>
                     <Button onClick={decrementQuantity}>-</Button>
-                    <Text fontSize='xs' p={2}>{quantity}</Text>
+                    <Text fontSize={{base: 'xs', md: 'sm'}} p={2}>{quantity}</Text>
                     <Button onClick={incrementQuantity}>+</Button>
                 </ButtonGroup>
-            </Box>
+            </Flex>
             <Box width='20%'>
-                <Text fontSize='sm' fontWeight={400}>{line_total.formatted_with_symbol}</Text>
+                <Text fontSize={{base: 'xs', md: 'sm'}} fontWeight={500}>{line_total.formatted_with_symbol}</Text>
             </Box>
             <Box>
-                <CloseButton onClick={removeItem}/>
+                <CloseButton size='md' onClick={removeItem}/>
             </Box>
         </Flex>
+        <Divider />
+        </>
     );
 }
 
@@ -59,19 +87,24 @@ export default function CartPage() {
     return (
         <>
         <Navbar textColor='black' />
-            <Text fontSize='2xl' px={{base: 2, md: 7}} mt={5} fontWeight={600}>Shopping Cart</Text>
-            <Flex flexWrap='wrap' justifyContent='space-between' width='100%'>
+        <Box px={{base: 5, md: 10}} mt={5}>
+            <Box pb={5}>
+                <Text fontSize='2xl' fontWeight={600}>Shopping Cart</Text>
+                <Text fontSize='sm'>{line_items.length} items</Text>
+            </Box>
                 {isEmpty ? (
-                    <Box>Your cart is empty!</Box>
+                    <Flex direction='column' align='center' justifyContent='center' m={5}>
+                        <Text fontSize='xl' fontWeight={500}>Your cart is empty!</Text>
+                    </Flex>
                 ) : (
                     // Items in cart view
-                    <>
-                        <Flex flexDirection='column' justifyContent='space-between' px={{base: 0, md: 5}} minWidth={{base: '100%', md: '70%'}}>
+                    <Flex flexWrap='wrap' justifyContent='space-between' width='100%'>
+                        <Flex flexDirection='column' justifyContent='space-between' minWidth={{base: '100%', md: '70%'}}>
                             {line_items.map((item) => (
                                 <CartItem key={item.id} {...item} />
                             ))}
                         </Flex>
-                        <Flex flexDirection='column' minWidth={{base: '100%', md: '30%'}} px={{base: 3, md: 7}} mt={{base: 2, md: 0}}>
+                        <Flex flexDirection='column' minWidth={{base: '100%', md: '30%'}} pl={5} mt={{base: 2, md: 0}}>
                             <Text fontSize='lg' fontWeight={600}>Order summary</Text>
                             <Divider />
                             <Box my={2}>
@@ -90,10 +123,10 @@ export default function CartPage() {
                             </Box>
                             <Button mb={5}>Checkout</Button>
                         </Flex> 
-                    </>
+                    </Flex>
                     )
                 }
-            </Flex>
+        </Box>
         </>
     );
 };
