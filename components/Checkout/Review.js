1c99971580c71872f0e69923451fecd6cef9dd5e
Jen Walker <jennie.walker@uconn.edu>
2021-08-23 12:02:02 -0700

    passing chekoutToken prop

--- components/Checkout/Review.js
+++ components/Checkout/Review.js
@@ -1,10 +1,31 @@
-import { Box } from '@chakra-ui/react';
+import { Box, Flex, Text, Divider, Button } from '@chakra-ui/react';
 
-export default function Review() {
+export default function Review({ checkoutToken }) {
 
     return (
-        <Box>
-
-        </Box>
+        <Flex minWidth={{base: '100%', md: '40%'}} height='200px' flexDirection='column' pl={{base: 0, md: 5}} borderRadius='md'>
+                    <Text fontSize='lg' fontWeight={600}>Order summary</Text>
+                    <Divider />
+                    <Box my={2}>
+                        <Flex mb={1} justifyContent='space-between'>
+                            <Text fontWeight='light'>Subtotal:</Text> 
+                            <Text fontWeight='semibold'>-</Text>
+                        </Flex>
+                        <Flex mb={1} justifyContent='space-between'>
+                            <Text fontWeight='light'>Tax:</Text> 
+                            <Text>TBD</Text>
+                        </Flex>
+                        <Flex mb={1} justifyContent='space-between'>
+                            <Text fontWeight='light'>Shipping:</Text> 
+                            <Text>TBD</Text>
+                        </Flex>
+                        <Divider mb={1}/>
+                        <Flex mb={1} justifyContent='space-between'>
+                            <Text fontWeight='semibold'>Total:</Text> 
+                            <Text fontWeight='semibold'>-</Text>
+                        </Flex>
+                    </Box>
+                    <Button isFullWidth>Complete order</Button>
+                </Flex>
     )
 }
--- components/Checkout/ShippingForm.js
+++ components/Checkout/ShippingForm.js
@@ -1,6 +1,7 @@
-import { Box, Input, Flex } from '@chakra-ui/react';
+import { Box, Input, Flex, Select, Button } from '@chakra-ui/react';
+import commerce from '../../lib/commerce';
 
-export default function ShippingForm() {
+export default function ShippingForm({ checkoutToken }) {
 
     return (
         <Box>
@@ -8,7 +9,21 @@ export default function ShippingForm() {
                 <Input m={1} placeholder='First name' />
                 <Input m={1} placeholder='Last name' />
             </Flex>
-
+            <Flex>
+                <Input m={1} placeholder='Email address' />
+            </Flex>
+            <Flex>
+                <Select m={1} placeholder='Country' />
+            </Flex>
+            <Flex>
+                <Input m={1} placeholder='Address line 1' />
+                <Input m={1} placeholder='Address line 2' />
+            </Flex>
+            <Flex mb={5}>
+                <Select width='50%' m={1} placeholder='State/Province' />
+                <Input width='50%' m={1} placeholder='Postal code' />
+            </Flex>
+            <Button isFullWidth>Confirm shipping information</Button>
         </Box>
     )
 }
