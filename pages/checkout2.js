8880216fe9a821c25e98632ee86c259d3c17b47c
Jen Walker <jennie.walker@uconn.edu>
2021-08-27 15:11:00 -0700

    new checkout page

--- pages/checkout2.js
+++ pages/checkout2.js
@@ -1,6 +1,11 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import commerce from '../lib/commerce';
-import { Box, Text, Flex } from '@chakra-ui/react'
+import { Box, Text, Flex, Button, Tabs, Tab, TabList, TabPanel, TabPanels } from '@chakra-ui/react'
 import { useCartState, useCartDispatch } from '../context/cart';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Navbar } from '../components';
+import { ArrowBackIcon } from '@chakra-ui/icons';
 import { useCheckoutState, useCheckoutDispatch } from '../context/checkout';
 
 export default function Checkout2() {
@@ -9,9 +14,29 @@ export default function Checkout2() {
     const { id, live } = useCheckoutState();
     const { generateToken, captureOrder, setError } = useCheckoutDispatch();
 
+    useEffect(() => {
+        generateToken(cartId);
+        console.log(id);
+    }, [cartId]);
+
     return (
         <Box>
-
+            <Navbar textColor='black' />
+            {/* Checkout */}
+            <Flex mx={5}>
+                {/* Header */}
+                <Flex mt={5}>
+                    {/* Back to cart button */}
+                    <Link href='/cart'>
+                        <a>
+                            <Button size='sm' variant='outline' leftIcon={<ArrowBackIcon />}>Back to cart</Button>
+                        </a>
+                    </Link>
+                    <Flex alignItems='center' justifyContent='center'>
+                        <Text>Checkout</Text>
+                    </Flex>
+                </Flex>
+            </Flex>
         </Box>
     )
 }
