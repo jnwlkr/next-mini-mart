d0be0fdbda02e0aa954f90a79a4e102a457ac695
Jen Walker <jennie.walker@uconn.edu>
2021-08-27 19:36:03 -0700

    installed react-hook-form

--- components/Checkout2/ShippingForm.js
+++ components/Checkout2/ShippingForm.js
@@ -1,10 +1,23 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, FormControl, Input, Select } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import { useCheckoutState, useCheckoutDispatch } from '../../context/checkout';
+import commerce from '../../lib/commerce';
+import { useEffect, useState } from 'react';
 
 export default function ShippingForm() {
+    const { id } = useCheckoutState();
+    const { setShippingMethod } = useCheckoutDispatch();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data) // shippingData
+    }
 
     return (
         <Flex>
-            ShippingForm
+            <form onSubmit={handleSubmit(onSubmit)}>
+
+            </form>
         </Flex>
     )
 }
