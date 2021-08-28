003c39e8a2da2fbfc589d6f70f4332b09c0edbbb
Jen Walker <jennie.walker@uconn.edu>
2021-08-27 19:03:26 -0700

    loading condition

--- components/Checkout2/OrderSummary.js
+++ components/Checkout2/OrderSummary.js
@@ -1,10 +1,22 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Text, Box, Divider } from '@chakra-ui/react';
+import { useCheckoutState } from '../../context/checkout';
 
 export default function OrderSummary() {
+    const { live } = useCheckoutState();
 
     return (
-        <Flex w='100%' borderRadius='md' boxShadow='xl' height='500px' bgColor='gray.100'>
-            Summary
+        <Flex w='100%' borderRadius='md' boxShadow='xl' bgColor='gray.50' ml={{base: 0, md: 5}} height='200px' direction='column'>
+            <Text fontSize='lg' p={5} color='blackAlpha.800' fontWeight={500}>Order Summary</Text>
+            <Box mx={5}>
+                <Flex justifyContent='space-between'>
+                    <Text fontWeight='light'>Subtotal:</Text> 
+                    <Text>{live.subtotal.formatted_with_symbol}</Text>
+                </Flex>
+                <Flex justifyContent='space-between'>
+                    <Text fontWeight='light'>Tax:</Text>
+                    <Text>{live.tax.amount.formatted_with_symbol}</Text>
+                </Flex>
+            </Box>
         </Flex>
     )
 }
--- pages/checkout2.js
+++ pages/checkout2.js
@@ -1,10 +1,10 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import commerce from '../lib/commerce';
-import { Box, Text, Flex, Button, Tabs, Tab, TabList, TabPanel, TabPanels } from '@chakra-ui/react';
+import { Box, Text, Flex, Button, Tabs, Tab, TabList, TabPanel, TabPanels, CircularProgress } from '@chakra-ui/react';
 import { useCartState, useCartDispatch } from '../context/cart';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { CheckoutNavbar, Form, OrderSummary } from '../components';
+import { CheckoutNavbar, Form, OrderSummary, Shipping, Payment } from '../components';
 import { useCheckoutState, useCheckoutDispatch } from '../context/checkout';
 
 export default function Checkout2() {
@@ -18,14 +18,35 @@ export default function Checkout2() {
         console.log(id);
     }, [cartId]);
 
+    if (!id) {
+        return (
+            <Flex direction='column' w='100hw' h='100vh' align='center' justify='center'>
+                <CircularProgress isIndeterminate /> Preparing checkout...
+            </Flex>
+        )
+    }
+
     return (
         <Box>
             <CheckoutNavbar />
             <Flex flexWrap='wrap' mx={{base: 5, md: 10}} mt={5}>
-                <Flex width={{base: '100%', md: '70%'}}>
-                    <Form />
-                </Flex>
-                <Flex width={{base: '100%', md: '30%'}}>
+                <Box width={{base: '100%', md: '60%'}} bgColor='gray.50' borderRadius='md' boxShadow='xl' pt={2}>
+                    <Tabs isFitted>
+                        <TabList>
+                            <Tab>1. Shipping</Tab>
+                            <Tab>2. Billing</Tab>
+                        </TabList>
+                        <TabPanels>
+                            <TabPanel>
+                                <Shipping />
+                            </TabPanel>
+                            <TabPanel>
+                                <Payment />
+                            </TabPanel>
+                        </TabPanels>
+                    </Tabs>
+                </Box>
+                <Flex width={{base: '100%', md: '40%'}}>
                     <OrderSummary />
                 </Flex>
                 
